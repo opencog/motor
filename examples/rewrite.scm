8796eb37fee2bc9eceaf3ce31108a270ac002844
Linas Vepstas <linas@linas.org>
2024-12-21 20:52:01 -0600

    Several copy-in variants

--- examples/rewrite.scm
+++ examples/rewrite.scm
@@ -14,5 +14,24 @@
 
 (cog-execute! (Write fs-handle (Item "ls")))
 
+(define copy-in
+	(Filter
+		(Rule
+			(Variable "$string-url")
+			(LinkSignature (Type 'StringValue)
+				(Variable "$string-url"))
+			(Variable "$string-url"))
+	 (Write fs-handle (Item "ls"))))
+
+(define copy-in
+	(Filter
+		(Rule
+			(Variable "$string-url")
+			(LinkSignature (Type 'StringValue)
+				(Variable "$string-url"))
+			(LinkSignature (Type 'LinkValue)
+				(Variable "$string-url")))
+	 (Write fs-handle (Item "ls"))))
+
 
 
