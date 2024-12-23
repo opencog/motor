9f390069542e44b30ece2c42aa1d3f160e6649a6
Linas Vepstas <linas@linas.org>
2024-12-23 15:00:01 -0600

    Work on the example some more

--- demos/crawler.scm
+++ demos/crawler.scm
@@ -1,8 +1,9 @@
 ;
 ; crawler.scm -- crawler demo
 ;
-; Pre-requisites: study the AtomSpace example
+; Pre-requisites: study the AtomSpace examples
 ; `examples/pattern-matcher/filter-value.scm`
+; `examples/pattern-matcher/filter-strings.scm`
 ;
 (use-modules (srfi srfi-1))
 (use-modules (opencog) (opencog exec) (opencog sensory))
@@ -15,24 +16,40 @@
 
 (define fs-handle (ValueOf (Anchor "xplor") (Predicate "fsys")))
 
-(define copy-in
-	(Filter
-		(Rule
-			(Variable "$string-url")
-			(LinkSignature (Type 'LinkValue)
-				(Variable "$string-url")
-				(LinkSignature (Type 'StringValue)
-					(Node "reg")))
+; The (Item "special") on the FileSys stream marks the files in the
+; stream with the file-type. This is "reg" for regular files, "dir"
+; for directories, and yet other types for fifos, sockets, char and
+; block devs. The rule below accepts only regular files, and records
+; the corresponding URL in the AtomSpace.
+;
+; I think that this rule is strict enough that it does nothing if it
+; is fed garbage of the wrong type. But it also does not do anything
+; special to avoid garbage: it it is attached incorrectly, it will do
+; the usual "garbage in, garbage out" dance.
+(define record-regular-file-url
+	(Rule
+		(Variable "$string-url")
 
-			; The rewrite.
-			(Edge
-				(Predicate "fURL")
-				(StringOfLink (Type 'ItemNode)
-					(Variable "$string-url"))))
-	 (Write fs-handle (Item "special"))))
+		; Accept only regular files
+		(LinkSignature (Type 'LinkValue)
+			(Variable "$string-url")
+			(StringOf (Type 'StringValue) (Node "reg")))
+
+		; Just tag the path as being a URL
+		(Edge
+			(Predicate "URL")
+			(StringOfLink (Type 'ItemNode)
+				(Variable "$string-url")))))
+
+; Wire in the rule defined above to the crawler source.
+(define get-regular-files
+	(Filter
+		record-regular-file-url
+		(Write fs-handle (Item "special"))))
 
 ; Try it.
-(cog-execute! copy-in)
+(cog-execute! get-regular-files)
+
 
 ; The directory entries are now searchable as conventional atoms.
 (define query
