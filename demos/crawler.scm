0db2c820172dbdde5b86b8aeb5a077a5472a109b
Linas Vepstas <linas@linas.org>
2024-12-23 20:04:08 -0600

    Work on the demo

--- demos/crawler.scm
+++ demos/crawler.scm
@@ -8,13 +8,28 @@
 (use-modules (srfi srfi-1))
 (use-modules (opencog) (opencog exec) (opencog sensory))
 
-; Open stream
-(cog-execute!
-   (SetValue
-      (Anchor "xplor") (Predicate "fsys")
-      (Open (Type 'FileSysStream) (Sensory "file:///tmp"))))
+; A file-system observer. Executing this will open the stream.
+(define fstream-observer
+	(Open (Type 'FileSysStream) (Sensory "file:///tmp")))
 
-(define fs-handle (ValueOf (Anchor "xplor") (Predicate "fsys")))
+; Location where we will put the bottom-level observer.
+; Optional, we don't really need this, but perhaps its
+; convenient for ... ??? something ???
+(define fstream-loc (ValueOf (Anchor "crawler") (Predicate "filestream")))
+
+; Associate the observer with the above location.
+; Optional, we don't really need this, but perhaps its
+; convenient for ... ??? something ???
+; Executing this will cause the stream to be opened,
+; and placed at the given location.
+(define fstream-trigger
+	(SetValue
+		(Anchor "crawler") (Predicate "filestream")
+		fstream-observer))
+
+; Open the stream, and actuall place it at the location.
+; Optional We don't need to do this yet (or at all).
+(cog-execute! fstream-trigger)
 
 ; The (Item "special") on the FileSys stream marks the files in the
 ; stream with the file-type. This is "reg" for regular files, "dir"
@@ -45,7 +60,17 @@
 (define get-regular-files
 	(Filter
 		record-regular-file-url
-		(Write fs-handle (Item "special"))))
+		(Write fstream-observer (Item "special"))))
+
+; Copy the names of the regular files into the AtomSpace
+; executing this runs the chain of sensory nodes and filters
+; defined above. We don't need to run this yet, but why not?
+(cog-execute! get-regular-files)
+
+(cog-execute!
+   (SetValue
+      (Anchor "crawler") (Predicate "reg-files")
+		get-regular-files))
 
 ; Try it.
 (cog-execute! get-regular-files)
